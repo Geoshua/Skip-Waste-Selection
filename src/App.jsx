7dc600bd4437aeac937d3a8b9770ea07cea92d50
PopuaL <lawjoshua@gmail.com>
2025-06-11 02:18:16 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-    <div className="h-full">
+    <div className="lg:h-full">
       <div className="w-full p-10 float-start">
 
         <SteppedProgressBar currentStep={step} />
